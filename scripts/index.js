8ca4538623c14d36c36c53513aac1609f4083390
Sergey Melihov <melih.serezha@gmail.com>
2022-08-08 12:58:39 +0300

    Внесение правок

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,37 @@
 let page = document.querySelector('.page');
 let profileUser = page.querySelector('.profile');
 let userName = profileUser.querySelector('.profile__text-label');
-let userProf = profileUser.querySelector('.profile__text-paragraf');
+let userProf = profileUser.querySelector('.profile__text-paragraph');
 let editingBtn = page.querySelector('.profile__edit-button');
-let closeBtn = page.querySelector('.popup__close-icon-btn');
+let closeBtn = page.querySelector('.popup__close-icon');
 let popup = page.querySelector('.popup');
 
 let popupInput = popup.querySelectorAll('.popup__input');
-let formElement = page.querySelector('.popup__box');
+let formElement = page.querySelector('.popup__form');
 
-let saveBtn = popup.querySelector('.popup__button-rectangle');
+let saveBtn = popup.querySelector('.popup__button-save');
 
 let heardLike = page.querySelectorAll('.element__like');
 
-function likeActiv(i) {
-    if (heardLike[i].classList.contains('like_activ') === true) {
-        heardLike[i].classList.remove('like_activ');
-    } else {
-        heardLike[i].classList.add('like_activ');
-    }
-}
-function init(){
-	for (let i = 0; i < heardLike.length; i++) {
-        heardLike[i].addEventListener('click', function(){ likeActiv(i); });
-    }
-}
 
-init();
+// ↓↓↓↓↓ Функция "Лайка", замена с прозрачного на черное ↓↓↓↓
+
+// function likeActive(i) {
+//     if (heardLike[i].classList.contains('like_active') === true) {
+//         heardLike[i].classList.remove('like_active');
+//     } else {
+//         heardLike[i].classList.add('like_active');
+//     }
+// }
+// function init(){
+// 	for (let i = 0; i < heardLike.length; i++) {
+//         heardLike[i].addEventListener('click', function(){ likeActive(i); });
+//     }
+// }
+
+// init();
+
+
 
 popupInput[0].value = userName.textContent;
 popupInput[1].value = userProf.textContent;
@@ -41,12 +46,12 @@ function openClosePopup() {
 
 editingBtn.addEventListener('click', openClosePopup);
 closeBtn.addEventListener('click', openClosePopup);
-saveBtn.addEventListener('click', openClosePopup);
+
 
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
-function formSubmitHandler (evt) {
+function formSubmitHandler(evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
                                                 // Так мы можем определить свою логику отправки.
                                                 // О том, как это делать, расскажем позже.
@@ -57,9 +62,9 @@ function formSubmitHandler (evt) {
     userName.textContent = popupInput[0].value;
     userProf.textContent = popupInput[1].value;
     // Вставьте новые значения с помощью textContent
+    openClosePopup()
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler); 
-
